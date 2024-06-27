c263fbf5d229afe92a2d4df0a482b67b2b328ba3
Steve <34465153+xxl4@users.noreply.github.com>
2024-06-27 16:35:25 +0800

    fix src view

---
+++ src/Http/Controllers/Admin/CustomersController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace NexaMerchant\Shopify\Http\Controllers\Admin;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+
+class CustomersController extends Controller
+{
+    public function index(Request $request) {
+        $data = [];
+        $data['code'] = 200;
+        $data['message'] = "Demo";
+        return view('Shopify::Admin.demo', compact("data"));
+    }
+}
---
+++ src/Resources/views/Admin/products/index.blade.php
@@ -0,0 +1,3 @@
+<x-admin::layouts>
+    
+</x-admin::layouts>
