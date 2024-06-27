c8e7c2fbae881e79989a0b911d69e2a37e715205
Steve <34465153+xxl4@users.noreply.github.com>
2024-06-27 17:31:08 +0800

    add testcase

---
+++ tests/ShopifyTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace NexaMerchant\Shopify\Tests;
+
+use PHPUnit\Event\Tracer\Tracer;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\TestCase;
+
+final class ShopifyTest extends TestCase
+{
+    public function testSubscriberRegistrationDoesNotWorkWhenEventFacadeIsSealed(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testTracerRegistrationDoesNotWorkWhenEventFacadeIsSealed(): void
+    {
+        $this->assertTrue(true);
+    }
+}
